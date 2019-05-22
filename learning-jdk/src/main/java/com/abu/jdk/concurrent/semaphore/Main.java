272036579541b265cecb0fd6b465d071caa2a1aa
zhewawan <wangzhe2222@163.com>
2019-05-23 07:21:47 +0800

    update semaphore

--- learning-jdk/src/main/java/com/abu/jdk/concurrent/semaphore/Main.java
+++ learning-jdk/src/main/java/com/abu/jdk/concurrent/semaphore/Main.java
@@ -2,6 +2,11 @@ package com.abu.jdk.concurrent.semaphore;
 
 import java.util.concurrent.Semaphore;
 
+/**
+ * https://howtodoinjava.com/java/multi-threading/control-concurrent-access-to-multiple-copies-of-a-resource-using-semaphore/
+ *
+ * 常用方法: 构造方法(n);构造方法(n,fair);acquire();release();availablePermits()
+ */
 public class Main {
     public static void main(String[] args) {
         int N = 8;            //工人数
@@ -22,6 +27,7 @@ public class Main {
         public void run() {
             try {
                 semaphore.acquire();
+                System.out.println(semaphore.availablePermits());
                 System.out.println("工人"+this.num+"占用一个机器在生产...");
                 Thread.sleep(2000);
                 System.out.println("工人"+this.num+"释放出机器");
